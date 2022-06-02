ba9a836ba2cc6942b43eac2eee4e21932173b9d6
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-06-02 16:08:28 +1000

    Added the code in index.js to pass all the test

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let multiply
+let num1 = 2, num2 = 1, num3 = 31;
+multiply = num1 * num2 * num3;
+
+let random
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod
+let num4 = 9, num5 = 5;
+mod = num4 % num5;
+
+let max = (1, 2, 3, 4, 10, 20);
+Math.max();
+
+
